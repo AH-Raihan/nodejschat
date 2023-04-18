94b5357557577a79113eaf65263a95fb0b90e087
AH-Raihan <web.ahraihan@gmail.com>
2023-04-19 05:24:35 +0600

    version 2

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const expressServer = http.createServer(app);
 
-const {Server} = require('socket.io');
+const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
-io.on('connection',(socket)=>{
-    socket.on('chat',(msg)=>{
-        io.emit('chat-transfer',[msg.textmsg,msg.userid]);
-    });
-    
-});
+let userslist = new Array();
+io.on("connection", (socket) => {
+  
+  socket.on("group-name", (groupName) => {
+    let groupn=groupName[0];
+    let usern=groupName[1];
 
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/index.html");
-    // res.end();
-});
-app.get('/sound',(req,res)=>{
-    res.sendFile(__dirname+"/Message notification.mp3");
+    // make group
+    socket.join(groupn);
+    let groupSize = io.sockets.adapter.rooms.get(groupName[0]).size;
+    io.emit(groupn, [groupSize, userslist]);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("leavemsg", "disconnected a user");
+  });
+
+  socket.on("chat", (msg) => {
+    io.sockets
+      .in(msg.groupid)
+      .emit("chat-transfer", [msg.textmsg, msg.userid, msg.username]);
+  });
 });
 
-expressServer.listen(3000,()=>{
-    console.log("server is runnig at http://localhost:3000");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/sound", (req, res) => {
+  res.sendFile(__dirname + "/Message notification.mp3");
+});
+app.use(express.static(__dirname + "/public"));
+
+expressServer.listen(3000, () => {
+  console.log("server is runnig at http://localhost:3000");
 });
